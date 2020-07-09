55a45bf15694e0a485888b1ebda4ec69367debe7
xuge <49184912@qq.com>
2020-07-10 00:00:09 +0800

    add: build files

---
+++ build/precache-manifest.c2a7265ea84c2c5ecaf5e7c94c646d68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb5232351398d5337824cf1cb62ec4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7904970c31b6a46a469d",
+    "url": "/static/css/main.70960b16.chunk.css"
+  },
+  {
+    "revision": "aec128df0a01aeb953d7",
+    "url": "/static/js/2.bd83a6b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bd83a6b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7904970c31b6a46a469d",
+    "url": "/static/js/main.e4f8c2bc.chunk.js"
+  },
+  {
+    "revision": "ff03d78d6bfc581fd98a",
+    "url": "/static/js/runtime-main.95797f02.js"
+  }
+]);
